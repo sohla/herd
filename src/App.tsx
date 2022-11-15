4fa5d8d46f6c5281c4db57d9fc00704ebd317c16
Steph OHara <soh_la@yahoo.com>
2022-11-16 08:54:31 +1100

    time-sync added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import { useCountdown, useTime, HOURS } from "react-time-sync";
+const cd = Date.now() + 4000
 
 function App() {
+  
+  const currentTime = useTime()
+  const secsDay = currentTime % 86400
+
+  const hours = Math.floor(secsDay / 3600) - 1
+  const minutes = Math.floor( (currentTime%3600) / 60)
+  const seconds = (currentTime%3600) % 60
+
+  const MyCountDown = (props: { until: number }) => {
+    const until =(props.until)
+    const timeLeft = useCountdown({ until })
+    return <div>{timeLeft > 0 ? `${timeLeft} seconds left` : "Done!"}</div>
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="bg-neutral-800 h-screen">
+
+      <Helmet>    
+        <meta name="viewport" content="user-scalable=no,initial-scale=1.0,maximum-scale=1.0" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Helmet>
+
+      <div className='flex flex-col h-screen justify-center w-full text-center text-white'>
+        <div className=' text-7xl ' >he.rd</div>
+        <div>Seconds : {currentTime}</div>
+        <div>Time in the day : {hours}:{minutes}:{seconds}</div>
+        <MyCountDown until={cd}/>
+      </div>
     </div>
   );
 }
